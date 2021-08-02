ad6adad5f8b4ab450d14e5e9215197daedd188c5
SAMSMU\dmakarichev <d.a.makarychev@samsmu.ru>
2021-08-02 15:21:07 +0400

    add onIn and onExit events for cell

--- Assets/Scripts/Container.cs
+++ Assets/Scripts/Container.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Container<T> : MonoBehaviour
 {
     [SerializeField] private T _content;
-    public virtual T Content { get => _content; set => _content = value; }
+
+    public virtual T Content
+    {
+        get => _content;
+        set
+        {
+            _content = value;
+            if (_content == null)
+            {
+                OnBecameEmpty.Invoke();
+            }
+            else
+            {
+                OnBecameFull.Invoke();
+            }   
+        }
+    }
 
     public bool isEmpty => Content == null;
 
+    public UnityEvent OnBecameFull;
+    public UnityEvent OnBecameEmpty;
 }
