a105541350ada9e0446b109545b50fb910bb7e79
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-08-06 23:40:31 +0400

    action points WIP

---
+++ Assets/Scripts/Containers/ActionPoints.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionPoints : MonoBehaviour
+{
+    [SerializeField] private IntContainer _currentPoints;
+    [SerializeField] private IntContainer _maxPointsCount;
+    [SerializeField] private IntContainer _pointsRegeneration;
+}
