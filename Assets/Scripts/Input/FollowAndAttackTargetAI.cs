38f0ea5f034df40588c4e67fb8634cd19ed1b530
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-08-13 17:27:18 +0400

    added follow target ai

---
+++ Assets/Scripts/Input/FollowAndAttackTargetAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class FollowAndAttackTargetAI : BaseInput
+{
+    [SerializeField] private Unit _target;
+    private List<Cell> _pathToTarget;
+
+    public override void Act()
+    {
+        if (_pathToTarget == null)
+            _pathToTarget = PathFinder.Instance.FindWay(_unit._currentCell, _target._currentCell);
+
+        _unit._movable.Move(_pathToTarget[0]);
+        if(_pathToTarget != null)
+            _pathToTarget.RemoveAt(0);
+    }
+
+    public void ClearPath()
+    {
+        _pathToTarget = null;
+    }
+}
