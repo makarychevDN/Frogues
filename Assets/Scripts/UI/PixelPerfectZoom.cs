171284094980fb91bf96a2995dfe8bbf1b469043
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-09-03 22:38:37 +0400

    UI folder scripts fields renamed

--- Assets/Scripts/UI/PixelPerfectZoom.cs
+++ Assets/Scripts/UI/PixelPerfectZoom.cs
@@ -7,17 +7,12 @@ using UnityEngine.UI;
 
 public class PixelPerfectZoom : MonoBehaviour
 {
-    [SerializeField] private PixelPerfectCamera _pixelPerfectCamera;
-    [SerializeField] private Slider _slider;
-
-    [SerializeField, ReadOnly]
-    private int[] xResolutions = new int[]
-    {
-        336, 420, 560, 840, 2200
-    };
+    [SerializeField] private PixelPerfectCamera pixelPerfectCamera;
+    [SerializeField] private Slider slider;
+    private int[] _xResolutions = new int[] { 336, 420, 560, 840, 2200 };
 
     public void UpdateResolution()
     {
-        _pixelPerfectCamera.refResolutionX = xResolutions[(int)_slider.value];
+        pixelPerfectCamera.refResolutionX = _xResolutions[(int)slider.value];
     }
 }
