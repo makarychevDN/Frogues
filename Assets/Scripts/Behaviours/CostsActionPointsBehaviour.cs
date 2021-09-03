714ca41d8c5db426b4eb14f933af4e760964265a
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-09-03 16:33:01 +0400

    behaviours folder scripts fields renamed

--- Assets/Scripts/Behaviours/CostsActionPointsBehaviour.cs
+++ Assets/Scripts/Behaviours/CostsActionPointsBehaviour.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class CostsActionPointsBehaviour : MonoBehaviour
 {
-    [SerializeField] private ActionPoints _actionPoints;
-    [SerializeField] private int _defaultActionPointsCost;
+    [SerializeField] private ActionPoints actionPoints;
+    [SerializeField] private int defaultActionPointsCost;
 
     public bool IsActionPointsEnough()
     {
-        return IsActionPointsEnough(_defaultActionPointsCost);
+        return IsActionPointsEnough(defaultActionPointsCost);
     }
 
     public bool IsActionPointsEnough(int actionPointsCost) 
     {
-        return _actionPoints.CheckIsActionPointsEnough(actionPointsCost);
+        return actionPoints.CheckIsActionPointsEnough(actionPointsCost);
     }
 
     public void SpendActionPoints()
     {
-        _actionPoints.SpendPoints(_defaultActionPointsCost);
+        actionPoints.SpendPoints(defaultActionPointsCost);
     }
 
     public void SpendActionPoints(int actionPointsCost)
     {
-        _actionPoints.SpendPoints(actionPointsCost);
+        actionPoints.SpendPoints(actionPointsCost);
     }
 }
