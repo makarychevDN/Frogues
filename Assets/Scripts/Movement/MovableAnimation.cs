a369ab19d96e7d98dd8b1a3bbd5cd544f647ba92
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-09-05 21:24:42 +0400

    some code refactoring

--- Assets/Scripts/Movement/MovableAnimation.cs
+++ Assets/Scripts/Movement/MovableAnimation.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 [RequireComponent(typeof(Movable))]
 public class MovableAnimation : CurrentlyActiveBehaviour
 {
-    [SerializeField] private Transform _sprite;
-    [SerializeField] private float speedInUnitsPerSecond;
+    [SerializeField] private Transform sprite;
+    [Range(0, 30), SerializeField] private float speedInUnitsPerSecond;
     [SerializeField] private float jumpHeight;
     [SerializeField] private AnimationCurve jumpCurve;
     private Cell _startCell, _targetCell;
     private float _currentTime, _totalTime, _distance;
     private bool _isPlaying;
-    private const float _spriteAlignment = 0.35f;
+    private float _spriteAlignment;
     private Movable _movable;
 
     void Start()
     {
+        _spriteAlignment = sprite.parent.localPosition.y;
         _totalTime = jumpCurve.keys[jumpCurve.keys.Length - 1].time;
         _movable = GetComponent<Movable>();
     }
@@ -35,9 +36,9 @@ public class MovableAnimation : CurrentlyActiveBehaviour
         if (!_isPlaying)
             return;
 
-        _sprite.position = Vector3.Lerp(_startCell.transform.position, _targetCell.transform.position, _currentTime);
-        _sprite.position += Vector3.up * _spriteAlignment;
-        _sprite.position += Vector3.up * jumpCurve.Evaluate(_currentTime) * jumpHeight;
+        sprite.position = Vector3.Lerp(_startCell.transform.position, _targetCell.transform.position, _currentTime);
+        sprite.position += Vector3.up * _spriteAlignment;
+        sprite.position += Vector3.up * jumpCurve.Evaluate(_currentTime) * jumpHeight;
         TimerStep();
     }
 
@@ -49,7 +50,7 @@ public class MovableAnimation : CurrentlyActiveBehaviour
             _currentTime = 0;
             _isPlaying = false;
             ActiveNow = false;
-            _sprite.localPosition = Vector3.zero;
+            sprite.localPosition = Vector3.zero;
             _movable.StopMovement(_targetCell);
         }
     }
