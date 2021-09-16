0376edffe1cf7f9f07fb119c543f85f359e2c691
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-09-16 23:17:47 +0400

    weapo wip

--- Assets/Scripts/Containers/Weapon.cs
+++ Assets/Scripts/Containers/Weapon.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Weapon : CostsActionPointsBehaviour
 {
-    //coming soon
     [SerializeField] private List<BaseCellsEffect> cellEffects;
+    [SerializeField] private BaseCellsTaker validCellTaker;
+    [SerializeField] private BaseCellsTaker selectedCellTaker;
 
     public void Use()
     {
-        cellEffects.ForEach(effect => effect.ApplyEffect());
+        var cells = selectedCellTaker.Take().Where(selectedCell => validCellTaker.Take().Contains(selectedCell)).ToList();
+        cellEffects.ForEach(effect => effect.ApplyEffect(cells));
     }
 }
