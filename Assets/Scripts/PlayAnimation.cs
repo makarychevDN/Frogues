f1f81ddf2d58db7cf8875f623138f56eca1649d1
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-09-20 00:01:02 +0400

    add Animation Player script

---
+++ Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : CurrentlyActiveBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private AnimationClip animationClip;
+    [SerializeField] private AnimationClip defaultAnimatorState;
+
+    public void Play()
+    {
+        animator.Play(animationClip.name);
+    }
+
+    public void Update()
+    {
+        //check animation already played
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            animator.Play(defaultAnimatorState.name);
+            ActiveNow = false;
+        }
+    }
+}
