e2446889627ad5938beefdb9fa854a8623b7b27d
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-09-28 00:30:34 +0400

    animation replaying on one frame bugfixed (again)

--- Assets/Scripts/PlayAnimation.cs
+++ Assets/Scripts/PlayAnimation.cs
@@ -19,7 +19,7 @@ public class PlayAnimation : CurrentlyActiveBehaviour
     public void Update()
     {
         //checks if THIS animation was played last + ANY animation already played
-        if (animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == animationClip.name && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9)
+        if (animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == animationClip.name && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.99f)
         {
             animator.Play(idleAnimation.name);
             ActiveNow = false;
