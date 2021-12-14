88934cf413e65038a827c296ea92c077853b1673
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-12-14 22:20:31 +0400

    usless inheritance removed

--- Assets/Scripts/Input/BaseInput.cs
+++ Assets/Scripts/Input/BaseInput.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class BaseInput : CurrentlyActiveBehaviour
+public abstract class BaseInput : MonoBehaviour
 {
     [SerializeField] protected Unit unit;
     public abstract void Act();
