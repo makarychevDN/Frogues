3c3697cde234f92a0ab11493d9d34653cf021310
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2022-01-11 17:00:35 +0400

    unit's description and description tags added

---
+++ Assets/Scripts/Units Description/DescriptionTag.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Description", menuName = "ScriptableObjects/Description Tag", order = 2)]
+public class DescriptionTag : ScriptableObject
+{
+    [SerializeField] private string text;
+    public string Text => text;
+    [SerializeField] private DescriptionTag descriptionTag;
+    public bool hasDescription => descriptionTag != null;
+    public Color textColor = Color.white;
+}
