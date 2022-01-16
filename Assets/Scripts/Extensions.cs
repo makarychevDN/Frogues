d51670b0882cedb15420c666a9b29aac64e4c19d
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2022-01-16 17:32:59 +0400

    get first and last element of list extensions added

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public static class Extensions 
+public static class Extensions
 {
     public static Vector3 ToVector3(this Vector2Int vector2Int)
     {
@@ -24,4 +25,8 @@ public static class Extensions
     {
         return cells.Where(cell => cell.Content != null).ToList();
     }
+
+    public static T GetFirst<T>(this List<T> list) => list[0];
+
+    public static T GetLast<T>(this List<T> list) => list[list.Count - 1];
 }
