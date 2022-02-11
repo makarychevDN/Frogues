85950d239220b8fd5d143428e7e079c876718e8c
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2022-02-11 13:34:07 +0400

    elipsis collider added

---
+++ Assets/Scripts/EllipsisCollider.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+public class EllipsisCollider : MonoBehaviour
+{
+    [SerializeField] private EdgeCollider2D edgeCollider2D;
+    [SerializeField, Range(2, 50)] private int smoothness;
+    [SerializeField] private float xRadius, yRadius;
+
+    private void OnValidate()
+    {
+        var calculatedPoints = new List<Vector2>(); 
+        edgeCollider2D = GetComponent<EdgeCollider2D>();
+        var deltaAngle = 360f / smoothness;
+        var deltaRadian = deltaAngle * Mathf.Deg2Rad;
+
+        for (int i = 0; i < smoothness; i++)
+        {
+            calculatedPoints.Add(new Vector2(Mathf.Cos(deltaRadian * i) * xRadius,Mathf.Sin(deltaRadian * i)) * yRadius);
+        }
+        calculatedPoints.Add(calculatedPoints[0]);
+
+        edgeCollider2D.points = calculatedPoints.ToArray();
+    }
+}
