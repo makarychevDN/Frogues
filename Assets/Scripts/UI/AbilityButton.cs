1232ba8497264f5d68ac8e9fafb7a68d5f211a8a
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2022-02-21 23:32:56 +0400

    new button materials

---
+++ Assets/Scripts/UI/AbilityButton.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityButton : MonoBehaviour
+{
+    [SerializeField] private AOEWeapon ability;
+    [SerializeField] private bool usingNow;
+    private Material _material;
+
+    private void Start()
+    {
+        _material = GetComponent<Image>().material;
+    }
+
+    public bool UsingNow
+    {
+        get => usingNow;
+        set
+        {
+            usingNow = value;
+            _material.SetInt("_AbilityUsingNow", value ? 1 : 0);
+        }
+    }
+    
+    
+
+}
