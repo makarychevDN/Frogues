c52e4307791b20fa72841a9bc2893fc311394225
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2022-02-23 15:12:53 +0400

    The button now highlights when using the ability

--- Assets/Scripts/UI/AbilityButton.cs
+++ Assets/Scripts/UI/AbilityButton.cs
@@ -2,17 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class AbilityButton : MonoBehaviour
+public class AbilityButton : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     [SerializeField] private AOEWeapon ability;
-    [SerializeField] private bool usingNow;
+    [SerializeField] private bool usingNow; 
+    private PlayerInput _playerInput;
     private Material _material;
 
     private void Awake()
     {
         _material = GetComponent<Image>().material;
+        _playerInput = FindObjectOfType<PlayerInput>();
     }
 
     public bool UsingNow
@@ -24,7 +27,35 @@ public class AbilityButton : MonoBehaviour
             _material.SetInt("_AbilityUsingNow", value ? 1 : 0);
         }
     }
-    
-    
 
+    private void Update()
+    {
+        _material.SetInt("_AbilityUsingNow", (_playerInput.currentAbility == ability) ? 1 : 0);
+    }
+
+    public void PickAbility()
+    {
+        _playerInput.currentAbility = ability;
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //throw new NotImplementedException();
+    }
 }
