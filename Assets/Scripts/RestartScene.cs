ac36652a8a0b32beff37545109abca581b7b94f9
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2022-05-03 20:05:08 +0400

    sound settings added

--- Assets/Scripts/RestartScene.cs
+++ Assets/Scripts/RestartScene.cs
@@ -4,14 +4,6 @@ namespace FroguesFramework
 {
     public class RestartScene : MonoBehaviour
     {
-        void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                Restart();
-            }
-        }
-
         public void Restart()
         {
             CurrentlyActiveObjects.Clear();
---
+++ Assets/Scripts/Settings/AudioVolumeSettings.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace FroguesFramework
+{
+    public class AudioVolumeSettings : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer audioMixer;
+        
+        public void SetMasterVolume(float value) => SetVolume("MasterVolume", value);
+        public void SetMusicVolume(float value) => SetVolume("MusicVolume", value);
+        public void SetSoundsVolume(float value) => SetVolume("SoundsVolume", value);
+        public void SetUIVolume(float value) => SetVolume("UIVolume", value);
+
+        private void SetVolume(string exposedParameterName, float value) => audioMixer.SetFloat(exposedParameterName, Mathf.Log(value) * 20);
+    }
+}
