8a55c688806897c79815bb484c655df568448edc
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2022-05-10 18:00:24 +0400

    new API added

--- Assets/Scripts/UnitSpawner.cs
+++ Assets/Scripts/UnitSpawner.cs
@@ -8,21 +8,28 @@ namespace FroguesFramework
         [SerializeField] private List<Unit> unitsToSpawn;
         [SerializeField] private BaseCellsTaker spawnCellsTaker;
 
-        public void Spawn()
+        public void Spawn() => Spawn(1);
+        
+        public void Spawn(IntContainer intContainer) => Spawn(intContainer.Content);
+
+        public void Spawn(int unitsQuantity)
         {
             if (spawnCellsTaker.Take() == null || spawnCellsTaker.Take().Count == 0)
                 return;
 
-            var spawnedUnit = Instantiate(unitsToSpawn[Random.Range(0, unitsToSpawn.Count)]);
-            var selectedCell = spawnCellsTaker.Take()[Random.Range(0, spawnCellsTaker.Take().Count)];
-            selectedCell =
-                Map.Instance.GetLayerByType(spawnedUnit.unitType)[selectedCell.coordinates.x,
-                    selectedCell.coordinates.y];
-            selectedCell.Content = spawnedUnit;
-            spawnedUnit.transform.position = selectedCell.transform.position;
+            for (int i = 0; i < unitsQuantity; i++)
+            {
+                var spawnedUnit = Instantiate(unitsToSpawn[Random.Range(0, unitsToSpawn.Count)]);
+                var selectedCell = spawnCellsTaker.Take()[Random.Range(0, spawnCellsTaker.Take().Count)];
+                selectedCell =
+                    Map.Instance.GetLayerByType(spawnedUnit.unitType)[selectedCell.coordinates.x,
+                        selectedCell.coordinates.y];
+                selectedCell.Content = spawnedUnit;
+                spawnedUnit.transform.position = selectedCell.transform.position;
 
-            if (spawnedUnit.input != null)
-                UnitsQueue.Instance.AddObjectInQueue(spawnedUnit);
+                if (spawnedUnit.input != null)
+                    UnitsQueue.Instance.AddObjectInQueue(spawnedUnit);
+            }
         }
     }
 }
