2c0fbe72542581c1806e9baf5482f6a69871409a
Даня <makarychev.dn@yandex.ru>
2022-09-11 21:23:59 +0400

    PlayAnimation,cs is working by AnimContainer now

--- Assets/Scripts/PlayAnimation.cs
+++ Assets/Scripts/PlayAnimation.cs
@@ -6,20 +6,23 @@ namespace FroguesFramework
     public class PlayAnimation : CurrentlyActiveBehaviour
     {
         [SerializeField] private Animator animator;
-        [SerializeField] private AnimationClip animationClip;
+        [SerializeField] private AnimationClipContainer animationClipContainer;
         [SerializeField] private AnimationClipContainer idleAnimationContainer;
         public UnityEvent OnAnimationPlayed;
 
         public void Play()
         {
-            animator.Play(animationClip.name);
+            animator.Play(animationClipContainer.Content.name);
             ActiveNow = true;
         }
 
         public void Update()
         {
+            if(animationClipContainer == null || animationClipContainer.Content == null)
+                return;
+            
             //checks if THIS animation was played last + ANY animation already played
-            if (animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == animationClip.name &&
+            if (animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == animationClipContainer.Content.name &&
                 animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.99f)
             {
                 animator.Play(idleAnimationContainer.Content.name);
