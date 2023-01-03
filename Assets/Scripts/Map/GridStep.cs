7e19f1851cd750edde64dd52f8b4a4e1cd9ca301
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-01-03 17:17:37 +0600

    cell size decreased + selected cell visualization updated

--- Assets/Scripts/Map/GridStep.cs
+++ Assets/Scripts/Map/GridStep.cs
@@ -2,8 +2,8 @@ namespace FroguesFramework
 {
     public static class GridStep
     {
-        public static float X => 1.7348f;
-        public static float Z => 1.5f;
+        public static float X => 1.7348f * 0.8f;
+        public static float Z => 1.5f * 0.8f;
     }
 
 }
