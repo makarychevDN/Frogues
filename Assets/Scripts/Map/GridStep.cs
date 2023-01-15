db2d0ca4894d6ce328a48cd8db5e42a818c9d556
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-01-15 21:32:35 +0600

    nano holes between tile object have been removed

--- Assets/Scripts/Map/GridStep.cs
+++ Assets/Scripts/Map/GridStep.cs
@@ -2,7 +2,7 @@ namespace FroguesFramework
 {
     public static class GridStep
     {
-        public static float X => 1.7348f * 0.8f;
+        public static float X => 1.7348f * 0.8f - 0.0025f;
         public static float Z => 1.5f * 0.8f;
     }
 
