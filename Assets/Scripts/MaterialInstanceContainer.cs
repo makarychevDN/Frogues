32fdf43193879d2d55861de7771f001e55ab8509
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-02-06 00:57:38 +0600

    new fire effect has been added

--- Assets/Scripts/MaterialInstanceContainer.cs
+++ Assets/Scripts/MaterialInstanceContainer.cs
@@ -1,26 +1,14 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI;
-
 
 namespace FroguesFramework
 {
     public class MaterialInstanceContainer : MonoBehaviour
     {
-        public Material materialPrefab;
         private Material _materialInstance;
-        public Color _color;
-        public Material MaterialPrefab => _materialInstance;
-        
+
         private void Start()
         {
-            _materialInstance = new Material(materialPrefab);
-            GetComponent<Image>().material = _materialInstance;
-        }
-
-        private void Update()
-        {
-            _materialInstance.color = _color;
+            _materialInstance = GetComponent<Renderer>().material;
         }
     }
 }
