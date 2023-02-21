8b524110bc0a1db9e0eabc11f2e6fd83a3aceb77
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-02-22 01:42:09 +0600

    cancer hermit deals damage to stepped on it unit from now

---
+++ Assets/Scripts/Abilities/DealDamageToSteppedOnThisUnitPassiveAbility.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace FroguesFramework
+{
+    public class DealDamageToSteppedOnThisUnitPassiveAbility : MonoBehaviour, IAbleToUseOnTarget, IAbility
+    {
+        [SerializeField] private int damage;
+        
+        public bool PossibleToUseOnTarget(Unit target)
+        {
+            return true;
+        }
+        
+        public void UseOnTarget(Unit target)
+        {
+            target.Health.TakeDamage(damage);
+        }
+
+        public void Init(Unit unit)
+        {
+            unit.OnStepOnThisUnitByTheUnit.AddListener(UseOnTarget);
+        }
+        
+        //todo need to remake interfaces stuff to remove this
+
+        public int GetCost()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsPartOfWeapon()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void VisualizePreUse()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Use()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
