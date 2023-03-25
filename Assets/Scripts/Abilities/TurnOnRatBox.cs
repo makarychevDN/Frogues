e5274f4fec50fba6ebd58a25a7cd65545af018fd
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-03-25 20:09:40 +0600

    rat with box has been added

---
+++ Assets/Scripts/Abilities/TurnOnRatBox.cs
@@ -0,0 +1,34 @@
+using FroguesFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FroguesFramework
+{
+    public class TurnOnRatBox : MonoBehaviour, IAbility
+    {
+        [SerializeField] private int armorValue;
+
+        public int GetCost() => 0;
+
+        public void Init(Unit unit)
+        {
+            unit.Health.IncreasePermanentArmor(armorValue);
+            unit.Animator.SetBool("BoxIsOn", true);
+            unit.Animator.SetTrigger(CharacterAnimatorParameters.ChangeWeapon);
+            unit.Health.OnBlockDestroyed.AddListener(() => unit.Animator.SetBool("BoxIsOn", false));
+        }
+
+        public bool IsPartOfWeapon() => false;
+
+        public void Use()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void VisualizePreUse()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
