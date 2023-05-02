f054531b2f369d7de4f44e6e368c25befa0759c4
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-05-02 20:27:32 +0600

    Cancer hermit abilities have been restored

---
+++ Assets/Scripts/Abilities/Ability Instanses/DealDamageOnContactWithUnitPassiveAbility.cs
@@ -0,0 +1,21 @@
+using FroguesFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamageOnContactWithUnitPassiveAbility : PassiveAbility
+{
+    [SerializeField] private int damage;
+    public override void Init(Unit unit)
+    {
+        base.Init(unit);
+
+        unit.OnStepOnThisUnitByTheUnit.AddListener(DealDamage);
+        unit.Movable.OnBumpIntoUnit.AddListener(DealDamage);
+    }
+
+    private void DealDamage(Unit unit)
+    {
+        unit.Health.TakeDamage(damage);
+    }
+}
