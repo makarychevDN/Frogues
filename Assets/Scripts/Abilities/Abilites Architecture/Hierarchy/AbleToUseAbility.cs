213f128e53c8f598749114e268e85d17fb6af93d
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-05-06 23:44:39 +0600

    well....

---
+++ Assets/Scripts/Abilities/Abilites Architecture/Hierarchy/AbleToUseAbility.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FroguesFramework
+{
+    public abstract class AbleToUseAbility : BaseAbility, IAbleToCost
+    {
+        [SerializeField] protected int cost;
+
+        [Header("Animation Setup")]
+        [SerializeField] protected AbilityAnimatorTriggers abilityAnimatorTrigger;
+        [SerializeField] protected WeaponIndexes weaponIndex = WeaponIndexes.NoNeedToChangeWeapon;
+        [SerializeField] protected float timeBeforeImpact;
+        [SerializeField] protected float fullAnimationTime;
+        [SerializeField] protected float delayBeforeImpactSound;
+        [SerializeField] protected AudioSource impactSoundSource;
+
+        public virtual bool IsActionPointsEnough() => _owner.ActionPoints.IsActionPointsEnough(cost);
+        public virtual int GetCost() => cost;
+        public virtual void SpendActionPoints() => _owner.ActionPoints.SpendPoints(cost);
+    }
+}
