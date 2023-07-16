1c0fc8e12479ab3283bdff2f37a0845b440f7410
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-07-16 19:48:38 +0600

    position and rotation clamps have been added

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 namespace FroguesFramework
@@ -9,7 +10,8 @@ namespace FroguesFramework
         [SerializeField] private float movementSpeed = 5f;
         [SerializeField] private float zoomingSpeed = 5f;
         private Transform _camera;
-        
+        [SerializeField] private float maxXPosition, minXPosition, maxZPosition, minZPosition;
+
         public void Init()
         {
             _camera = Camera.main.transform;
@@ -17,6 +19,11 @@ namespace FroguesFramework
             _camera.localPosition = Vector3.zero;
             _camera.localRotation = Quaternion.Euler(30f, 0, 0);
             _camera.localPosition -= Camera.main.transform.forward * distanceFromCameraToCenter;
+
+            maxXPosition = EntryPoint.Instance.Map.allCells.Max(cell => cell.transform.position.x);
+            minXPosition = EntryPoint.Instance.Map.allCells.Min(cell => cell.transform.position.x);
+            maxZPosition = EntryPoint.Instance.Map.allCells.Max(cell => cell.transform.position.z);
+            minZPosition = EntryPoint.Instance.Map.allCells.Min(cell => cell.transform.position.z);
         }
         
         void Update()
@@ -35,11 +42,16 @@ namespace FroguesFramework
         public void Move(Vector2 movement)
         {
             cameraRotationPoint.transform.localPosition += cameraRotationPoint.TransformDirection(new Vector3(movement.x, 0, movement.y)) * Time.deltaTime * movementSpeed;
+            var clampedXPosition = Mathf.Clamp(cameraRotationPoint.transform.position.x, minXPosition, maxXPosition);
+            var clampedZPosition = Mathf.Clamp(cameraRotationPoint.transform.position.z, minZPosition, maxZPosition);
+            cameraRotationPoint.transform.position = new Vector3(clampedXPosition, 0, clampedZPosition);
         }
         
         public void Zoom(float value)
         {
-            _camera.transform.position += _camera.forward * value * zoomingSpeed;
+            Vector3 updatedPosition = _camera.transform.position + _camera.forward * value * zoomingSpeed;
+            Vector3 clampedPosition = Extensions.ClampMagnitude(_camera.forward * (cameraRotationPoint.position - updatedPosition).magnitude, 10, 3);
+            _camera.transform.position = cameraRotationPoint.position - clampedPosition;
         }
     }
 }
--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -126,5 +126,13 @@ namespace FroguesFramework
 
             return (HexDir)dirValue;
         }
+
+        public static Vector3 ClampMagnitude(Vector3 v, float max, float min)
+        {
+            double sm = v.sqrMagnitude;
+            if (sm > (double)max * (double)max) return v.normalized * max;
+            else if (sm < (double)min * (double)min) return v.normalized * min;
+            return v;
+        }
     }
 }
