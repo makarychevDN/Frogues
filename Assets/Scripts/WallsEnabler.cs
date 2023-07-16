c7cc177019431fc9662a36bda2cf7a6af02edc9f
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-07-16 22:51:25 +0600

    the first version of the WallEnabler is ready

---
+++ Assets/Scripts/WallsEnabler.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FroguesFramework
+{
+    public class WallsEnabler : MonoBehaviour
+    {
+        [SerializeField] private CameraController cameraController;
+        [SerializeField] private List<Pillar> pillars;
+        [SerializeField] private List<GameObject> walls;
+        [SerializeField] private float angleDelta = 45f;
+
+        private void Update()
+        {
+            EnableWalls();
+            EnablePillars();
+        }
+
+        private void EnableWalls()
+        {
+            foreach (var wall in walls)
+            {
+                wall.SetActive(Vector3.Angle(Camera.main.transform.forward, wall.transform.right) > angleDelta);
+            }
+        }
+
+        private void EnablePillars()
+        {
+            foreach (var pillar in pillars)
+            {
+                pillar.itSelf.SetActive(pillar.neighborWalls.Any(wall => wall.activeInHierarchy));
+            }
+        }
+    }
+
+    [Serializable]
+    public struct Pillar
+    {
+        public GameObject itSelf;
+        public List<GameObject> neighborWalls;
+    }
+}
