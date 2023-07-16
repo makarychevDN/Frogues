5ee3abc7d5be22090f5c1096eec1b47a4fb7aef1
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-07-16 18:56:40 +0600

    rotation and zoom have been added + movement has been fixed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,30 +6,40 @@ namespace FroguesFramework
     {
         [SerializeField] private float distanceFromCameraToCenter = 5f;
         [SerializeField] private Transform cameraRotationPoint;
-        [SerializeField] private float speed = 5;
+        [SerializeField] private float movementSpeed = 5f;
+        [SerializeField] private float zoomingSpeed = 5f;
+        private Transform _camera;
         
         public void Init()
         {
-            Camera.main.transform.parent = cameraRotationPoint;
-            Camera.main.transform.localPosition = Vector3.zero;
-            Camera.main.transform.localPosition -= Camera.main.transform.forward * distanceFromCameraToCenter;
+            _camera = Camera.main.transform;
+            _camera.parent = cameraRotationPoint;
+            _camera.localPosition = Vector3.zero;
+            _camera.localRotation = Quaternion.Euler(30f, 0, 0);
+            _camera.localPosition -= Camera.main.transform.forward * distanceFromCameraToCenter;
         }
         
         void Update()
         {
             if (Input.GetKey(KeyCode.Mouse2))
             {
-                transform.Rotate(transform.up, Input.GetAxis("Mouse X"));
+                cameraRotationPoint.Rotate(cameraRotationPoint.up, Input.GetAxis("Mouse X"));
             }
 
             Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+            Zoom(Input.GetAxis("Mouse ScrollWheel"));
         }
 
         public void Deactivate() => Camera.main.transform.parent = null;
 
         public void Move(Vector2 movement)
         {
-            cameraRotationPoint.transform.localPosition += new Vector3(movement.x, 0, movement.y) * Time.deltaTime * speed;
+            cameraRotationPoint.transform.localPosition += cameraRotationPoint.TransformDirection(new Vector3(movement.x, 0, movement.y)) * Time.deltaTime * movementSpeed;
+        }
+        
+        public void Zoom(float value)
+        {
+            _camera.transform.position += _camera.forward * value * zoomingSpeed;
         }
     }
 }
