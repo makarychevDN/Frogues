fe30bc584d00a3c6208880a9c64ff1cf0b2e6b11
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-07-21 23:49:46 +0600

    Wall's enabler works correctly

--- Assets/Scripts/AnimatedWall.cs
+++ Assets/Scripts/AnimatedWall.cs
@@ -7,24 +7,29 @@ namespace FroguesFramework
     {
         [SerializeField] private bool showed = true;
         [SerializeField] private Animator animator;
-        private float _cooldown = 0.2f;
-        private bool _coldowned = true;
+        //private float _cooldown = 0.2f;
+        //private bool _coldowned = true;
 
         public bool Showed
         {
             get => showed;
             set 
             {
-                if (!_coldowned)
+                //if (!_coldowned)
+                //return;
+
+                //_coldowned = false;
+                //Invoke(nameof(ResetCooldown), _cooldown);
+
+                if (showed == value)
                     return;
 
-                _coldowned = false;
-                Invoke(nameof(ResetCooldown), _cooldown);
                 showed = value;
-                animator.SetTrigger(value ? "Show" : "Hide");
+                //animator.SetTrigger(value ? "Show" : "Hide");
+                animator.SetBool("IsShowing", value);
             }
         }
 
-        private void ResetCooldown() => _coldowned = true;
+        //private void ResetCooldown() => _coldowned = true;
     }
 }
