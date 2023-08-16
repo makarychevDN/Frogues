7ab7aebb99a2de12c81cf0a5d17b6e488e6f2e58
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-08-16 18:56:08 +0600

    healthbr visualization has been updated (now block draws right-to-left)

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -8,7 +8,8 @@ namespace FroguesFramework
     {
         [SerializeField] private Slider hpSlider;
         [SerializeField] private Slider armorSlider;
-        [SerializeField] private Slider preTakenDamageDeltaSlider;
+        [SerializeField] private Slider preTakenDamageHPSlider;
+        [SerializeField] private Slider preTakenDamageArmorSlider;
         [SerializeField] private Health health;
         [SerializeField] private TextMeshProUGUI textField;
 
@@ -16,11 +17,13 @@ namespace FroguesFramework
         {
             hpSlider.maxValue = health.MaxHp + health.Block;
             armorSlider.maxValue = health.MaxHp + health.Block;
-            preTakenDamageDeltaSlider.maxValue = health.MaxHp + health.Block;
+            preTakenDamageHPSlider.maxValue = health.MaxHp + health.Block;
+            preTakenDamageArmorSlider.maxValue = health.MaxHp + health.Block;
             
             hpSlider.value = health.HealthWithPreTakenDamage;
-            armorSlider.value = health.HealthWithPreTakenDamage + health.ArmorWithPreTakenDamage;
-            preTakenDamageDeltaSlider.value = health.CurrentHp + health.Block;
+            armorSlider.value = health.ArmorWithPreTakenDamage;
+            preTakenDamageHPSlider.value = health.CurrentHp;
+            preTakenDamageArmorSlider.value = health.Block;
 
             if (textField == null)
                 return;
