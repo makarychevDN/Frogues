692ac6760b3cc90a7ff3d571fc842c5347b78848
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-08-25 19:29:46 +0600

    legacy code has been removed

--- Assets/Scripts/UI/ResourcePointUI.cs
+++ Assets/Scripts/UI/ResourcePointUI.cs
@@ -5,63 +5,32 @@ namespace FroguesFramework
 {
     public class ResourcePointUI : MonoBehaviour
     {
-        [SerializeField] private GameObject fullResourcePointIcon;
-        [SerializeField] private GameObject preCostedResourcePoint;
-        [SerializeField] private GameObject emptyResourcePoint;
-        [SerializeField] private GameObject notEnoughResourcePointsIcon;
         [SerializeField] private Animator animator;
-        private List<GameObject> allIcons;
-
-        private void Start()
-        {
-            InitAllIcons();
-        }
-
-        private void InitAllIcons()
-        {
-            allIcons = new List<GameObject>
-            {
-                fullResourcePointIcon,
-                preCostedResourcePoint,
-                emptyResourcePoint,
-                notEnoughResourcePointsIcon
-            };
-            allIcons.RemoveAll(icon => icon == null);
-        }
 
         public void EnableFullIcon()
         {
-            if (animator != null)
-                animator.SetBool("Full", true);
+            animator.SetBool("Full", true);
         }
 
         public void EnablePreCostIcon()
         {
-            if (animator != null)
-            {
-                animator.SetBool("PreCost", true);
-                animator.SetTrigger("ResetPreCostBlinking");
-            }
+            animator.SetBool("PreCost", true);
+            animator.SetTrigger("ResetPreCostBlinking");
         }
 
         public void DisablePreCostIcon()
         {
-            if (animator != null)
-                animator.SetBool("PreCost", false);
+            animator.SetBool("PreCost", false);
         }
 
         public void EnableEmptyIcon()
         {
-            if (animator != null)
-                animator.SetBool("Full", false);
+            animator.SetBool("Full", false);
         }
 
         public void Regen()
         {
-            if(animator != null)
-                animator.SetTrigger("Regen");
+            animator.SetTrigger("Regen");
         }
-
-        private void DisableAllIcons() => allIcons?.ForEach(icon => icon.SetActive(false));
     }
 }
