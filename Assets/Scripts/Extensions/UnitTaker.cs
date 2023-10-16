4f68bf6a0479772b45dd3207d3ed6262341aebbb
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-10-16 17:59:00 +0600

    minor: some clean up in the folders

---
+++ Assets/Scripts/Extensions/UnitTaker.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FroguesFramework
+{
+    public static class UnitTaker
+    {
+        public static Unit TakeClosestUnitToUnitInRadius(this Unit unit, int radius)
+        {
+            List<Unit> closestUnits = new List<Unit>();
+            //closestUnits = unit.
+            return null;
+        }
+    }
+}
