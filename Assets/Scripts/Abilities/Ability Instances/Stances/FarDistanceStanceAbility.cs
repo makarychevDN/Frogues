2df25f58486be6c7b39581b1f6c765fbe20f7586
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-11-10 01:35:27 +0600

    more stances have been added

---
+++ Assets/Scripts/Abilities/Ability Instances/Stances/FarDistanceStanceAbility.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FroguesFramework
+{
+    public class FarDistanceStanceAbility : BattleStanceAbility
+    {
+        [SerializeField] private List<StatEffectAndDeltaWhenOnlyNoEnemieNearby> startEffectsAndDeltas;
+        private List<StatEffect> _effects;
+
+        public override void Init(Unit unit)
+        {
+            base.Init(unit);
+
+            _effects = new List<StatEffect>();
+            for (int i = 0; i < startEffectsAndDeltas.Count; i++)
+            {
+                var startValue = startEffectsAndDeltas[i].startValue;
+                _effects.Add(new StatEffect(startValue.type, startValue.Value, startValue.timeToTheEndOfEffect, startValue.effectIsConstantly));
+            }
+        }
+
+        public override void ApplyEffect(bool isActive)
+        {
+            base.ApplyEffect(isActive);
+
+            if (isActive)
+            {
+                RecalculateValue();
+                EntryPoint.Instance.OnSomeoneMoved.AddListener(RecalculateValue);
+
+                foreach (var effect in _effects)
+                {
+                    _owner.Stats.AddStatEffect(effect);
+                }
+            }
+            else
+            {
+                EntryPoint.Instance.OnSomeoneMoved.RemoveListener(RecalculateValue);
+
+                foreach (var effect in _effects)
+                {
+                    _owner.Stats.RemoveStatEffect(effect);
+                }
+            }
+        }
+
+        private void RecalculateValue()
+        {
+            int nearbyEnemiesQuantity = _owner.CurrentCell.CellNeighbours.GetAllNeighbors().Where(cell => cell.Content != null && cell.Content is not Barrier).Count();
+            int onlyOneEnemieNearbyModificator = nearbyEnemiesQuantity == 0 ? 1 : 0;
+
+            for (int i = 0; i < _effects.Count; i++)
+            {
+                _effects[i].Value = startEffectsAndDeltas[i].startValue.Value + onlyOneEnemieNearbyModificator * startEffectsAndDeltas[i].deltaForNoEnemyNearby;
+            }
+        }
+    }
+
+    [Serializable]
+    public class StatEffectAndDeltaWhenOnlyNoEnemieNearby
+    {
+        public int deltaForNoEnemyNearby;
+        public StatEffect startValue;
+    }
+}
