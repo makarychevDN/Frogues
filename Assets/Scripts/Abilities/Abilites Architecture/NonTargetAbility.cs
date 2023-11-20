a542aed6ed5604f91744b6cda39f3cb8529a540c
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-11-21 01:38:14 +0600

    bigfix: now nontarget ability check it's possibility to use

--- Assets/Scripts/Abilities/Abilites Architecture/NonTargetAbility.cs
+++ Assets/Scripts/Abilities/Abilites Architecture/NonTargetAbility.cs
@@ -2,7 +2,7 @@ namespace FroguesFramework
 {
     public abstract class NonTargetAbility : AbleToUseAbility, IAbleToUseWithNoTarget
     {
-        public virtual bool PossibleToUse() => _owner.ActionPoints.IsPointsEnough(bloodPointsCost);
+        public virtual bool PossibleToUse() => _owner.ActionPoints.IsPointsEnough(actionPointsCost) && _owner.BloodPoints.IsPointsEnough(bloodPointsCost);
         public abstract void Use();
     }
 }
