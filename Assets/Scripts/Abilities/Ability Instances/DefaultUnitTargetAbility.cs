4c5632b5bea3d51d990190918c1092cfc25d2c62
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2023-12-08 18:42:36 +0600

    new arrows have been integrated into abilities

--- Assets/Scripts/Abilities/Ability Instances/DefaultUnitTargetAbility.cs
+++ Assets/Scripts/Abilities/Ability Instances/DefaultUnitTargetAbility.cs
@@ -99,8 +99,8 @@ namespace FroguesFramework
             _owner.ActionPoints.PreSpendPoints(actionPointsCost);
             _owner.BloodPoints.PreSpendPoints(bloodPointsCost);
             lineFromOwnerToTarget.gameObject.SetActive(true);
-            lineFromOwnerToTarget.SetPosition(0, _owner.SpriteParent.position - _owner.transform.position);
-            lineFromOwnerToTarget.SetPosition(1, target.SpriteParent.position - _owner.transform.position);
+            lineFromOwnerToTarget.SetPosition(0, _owner.SpriteParent.position);
+            lineFromOwnerToTarget.SetPosition(1, target.SpriteParent.position);
         }
 
         public override void DisablePreVisualization()
--- Assets/Scripts/Abilities/Ability Instances/ExplodeBloodSurface.cs
+++ Assets/Scripts/Abilities/Ability Instances/ExplodeBloodSurface.cs
@@ -57,8 +57,8 @@ namespace FroguesFramework
             _owner.ActionPoints.PreSpendPoints(actionPointsCost);
             _owner.BloodPoints.PreSpendPoints(bloodPointsCost);
             lineFromOwnerToTarget.gameObject.SetActive(true);
-            lineFromOwnerToTarget.SetPosition(0, _owner.SpriteParent.position - _owner.transform.position);
-            lineFromOwnerToTarget.SetPosition(1, target.SpriteParent.position - _owner.transform.position);
+            lineFromOwnerToTarget.SetPosition(0, _owner.SpriteParent.position);
+            lineFromOwnerToTarget.SetPosition(1, target.SpriteParent.position);
 
             TakeUnitsAroundSurfaceTarget(target).ForEach(targetInTheArea => targetInTheArea.Health.PreTakeDamage(CalculateDamage, ignoreArmor));
         }
