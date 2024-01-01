876b3f7a4822efeb8174cc781bf212e1805a7d4a
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-01-01 22:22:49 +0600

    new unit's ui works have been started + new healthbar design has been added

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -10,6 +10,7 @@ namespace FroguesFramework
         [SerializeField] private Slider armorSlider;
         [SerializeField] private Slider emptyHPSlider;
         [SerializeField] private Slider preTakenDamageHPSlider;
+        [SerializeField] private Slider preTakenDamageAnimatedHPSlider;
         [SerializeField] private Slider preTakenDamageArmorSlider;
         [SerializeField] private Health health;
         [SerializeField] private TextMeshProUGUI textField;
@@ -26,10 +27,12 @@ namespace FroguesFramework
             armorSlider.maxValue = maxSlidersValue;
             emptyHPSlider.maxValue = maxSlidersValue;
             preTakenDamageHPSlider.maxValue = maxSlidersValue;
+            if(preTakenDamageAnimatedHPSlider != null) preTakenDamageAnimatedHPSlider.maxValue = maxSlidersValue;
             preTakenDamageArmorSlider.maxValue = maxSlidersValue;
             
             hpSlider.value = health.HealthWithPreTakenDamage;
             preTakenDamageHPSlider.value = health.CurrentHp;
+            if (preTakenDamageAnimatedHPSlider != null) preTakenDamageAnimatedHPSlider.value = health.CurrentHp;
             emptyHPSlider.value = health.MaxHp;
             armorSlider.value = health.BlockWithPreTakenDamage + health.MaxHp;
             preTakenDamageArmorSlider.value = health.Block + health.MaxHp;
