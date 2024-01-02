e19a7cf1dc088e103af3e6be3fc20238110528c2
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-01-02 17:42:11 +0600

    healthbar is able to disable useless icons now

--- Assets/Scripts/UI/AbilitiesPanelHealthBar.cs
+++ Assets/Scripts/UI/AbilitiesPanelHealthBar.cs
@@ -5,6 +5,10 @@ namespace FroguesFramework
 {
     public class AbilitiesPanelHealthBar : BaseHealthBar
     {
+        [SerializeField] private GameObject blockIcon;
+        [SerializeField] private GameObject armorIcon;
+        [SerializeField] private GameObject spikesIcon;
+
         [SerializeField] private TextMeshProUGUI healthTextField;
         [SerializeField] private TextMeshProUGUI blockTextField;
         [SerializeField] private TextMeshProUGUI armorTextField;
@@ -13,6 +17,11 @@ namespace FroguesFramework
         protected override void Redraw()
         {
             base.Redraw();
+
+            blockIcon.SetActive(health.TemporaryBlock != 0);
+            armorIcon.SetActive(health.PermanentBlock != 0);
+            spikesIcon.SetActive(stats.Spikes != 0);
+
             healthTextField.text = (health.CurrentHp).ToString();
             blockTextField.text = (health.TemporaryBlock).ToString();
             armorTextField.text = (health.PermanentBlock).ToString();
