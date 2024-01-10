a771f55d874b1dc04f54715568c0fc0bf6a27898
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-01-10 19:22:50 +0600

    a new passive ability has been added

--- Assets/Scripts/Abilities/Ability Instances/Passive Abilities/AddTemporaryActionPointIfEnemyNearbyOnTheEndOfTurn.cs
+++ Assets/Scripts/Abilities/Ability Instances/Passive Abilities/AddTemporaryActionPointIfEnemyNearbyOnTheEndOfTurn.cs
@@ -1,30 +1,64 @@
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace FroguesFramework
 {
-    public class AddTemporaryActionPointIfEnemyNearbyOnTheEndOfTurn : PassiveAbility
+    public class AddTemporaryActionPointIfEnemyNearbyOnTheEndOfTurn : PassiveAbility, IAbleToReturnSingleValue, IRoundTickable, IAbleToHighlightAbilityButton
     {
         [SerializeField] private int temporaryActionPointsValue;
+        private UnityEvent<bool> highlightEvent = new();
+        private bool lastTurnEndedNearbyEnemy;
 
         public override void Init(Unit unit)
         {
             base.Init(unit);
-            _owner.AbleToSkipTurn.OnSkipTurn.AddListener(TryToAddTemporaryPoint);
+            _owner.AbleToSkipTurn.OnSkipTurn.AddListener(HashValue);
+            EntryPoint.Instance.OnSomeoneMoved.AddListener(TryToHighlightButton);
         }
 
-        private void TryToAddTemporaryPoint()
+        private void HashValue()
         {
-            if(_owner.CurrentCell.CellNeighbours.GetAllNeighbors().Any(cell => cell.Content.IsEnemy))
-            {
-                _owner.ActionPoints.IncreaseTemporaryPoints(temporaryActionPointsValue);
-            }
+            lastTurnEndedNearbyEnemy = AnyEnemyNearby;
         }
+
+        private void TryToHighlightButton() => highlightEvent.Invoke(AnyEnemyNearby && EntryPoint.Instance.UnitsQueue.IsUnitCurrent(_owner));
+
+        private bool AnyEnemyNearby => _owner.CurrentCell.CellNeighbours.GetAllNeighbors().Any(cell => (!cell.IsEmpty && cell.Content.IsEnemy));
 
         public override void UnInit()
         {
             base.UnInit();
-            _owner.AbleToSkipTurn.OnSkipTurn.RemoveListener(TryToAddTemporaryPoint);
+            _owner.AbleToSkipTurn.OnSkipTurn.RemoveListener(HashValue);
+            EntryPoint.Instance.OnSomeoneMoved.RemoveListener(TryToHighlightButton);
         }
+
+        public int GetValue() => temporaryActionPointsValue;
+
+        public void TickAfterEnemiesTurn()
+        {
+            if (_owner.IsEnemy)
+                return;
+
+            if (lastTurnEndedNearbyEnemy)
+                _owner.ActionPoints.IncreaseTemporaryPoints(temporaryActionPointsValue);
+
+            lastTurnEndedNearbyEnemy = false;
+            highlightEvent.Invoke(AnyEnemyNearby);
+        }
+
+        public void TickAfterPlayerTurn()
+        {
+            if (!_owner.IsEnemy)
+                return;
+
+            if (lastTurnEndedNearbyEnemy)
+                _owner.ActionPoints.IncreaseTemporaryPoints(temporaryActionPointsValue);
+
+            lastTurnEndedNearbyEnemy = false;
+            highlightEvent.Invoke(AnyEnemyNearby);
+        }
+
+        public UnityEvent<bool> GetHighlightEvent() => highlightEvent;
     }
 }
