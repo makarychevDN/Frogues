fd2f19e02b331da35cb46689fd80c2efdc248cf3
dyst0p <dyst0p.gd@gmail.com>
2024-01-18 22:56:15 +0400

    [~] addad tags for descriptions

--- Assets/Scripts/Abilities/Ability Instances/GenericSelfBuffAbility.cs
+++ Assets/Scripts/Abilities/Ability Instances/GenericSelfBuffAbility.cs
@@ -1,24 +1,21 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace FroguesFramework
 {
-    public class GenericSelfBuffAbility : NonTargetAbility
-    {
-        [Serializable]
-        public struct BuffEffect
-        {
-            public StatEffectTypes BuffType;
-            public int BuffValue;
-            public int TimeToEndBuff;
-            public bool BuffIsConstantly;
-        }
+    public class GenericSelfBuffAbility : NonTargetAbility, IAbleToApplyStrenghtModificator,
+        IAbleToApplyIntelligenceModificator, IAbleToApplyDexterityModificator, IAbleToApplyDefenceModificator,
+        IAbleToApplySpikesModificator, IAbleToApplyImmobilizedModificator, IAbleToApplyBlock, IAbleToApplyArmor
 
-        [Space, Header("Ability Settings")]
-        [SerializeField] private int temporaryBlockValue;
+    {
+        [Space, Header("Ability Settings")] [SerializeField]
+        private int temporaryBlockValue;
+
         [SerializeField] private int permanentBlockValue;
-        [SerializeField] private BuffEffect[] Buffs;
+        [SerializeField] private List<StatEffect> effects;
 
         public override void Use()
         {
@@ -37,17 +34,82 @@ namespace FroguesFramework
         protected virtual IEnumerator ApplyEffect(float time)
         {
             yield return new WaitForSeconds(time);
-            
+
             if (temporaryBlockValue != 0)
                 _owner.Health.IncreaseTemporaryBlock(temporaryBlockValue);
             if (permanentBlockValue != 0)
                 _owner.Health.IncreasePermanentBlock(permanentBlockValue);
-            
-            foreach (BuffEffect buff in Buffs)
-                _owner.Stats.AddStatEffect(new StatEffect(buff.BuffType, buff.BuffValue, buff.TimeToEndBuff,
-                    effectIsConstantly: buff.BuffIsConstantly));
+
+            foreach (StatEffect buff in effects)
+                _owner.Stats.AddStatEffect(buff);
         }
 
         private void RemoveCurrentlyActive() => CurrentlyActiveObjects.Remove(this);
+
+        #region IAbleToApplyDefenceModificator
+        public int GetDefenceModificatorValue() => Extensions.GetModificatorValue(effects, StatEffectTypes.defence);
+
+        public int GetdeltaOfDefenceValueForEachTurn() => Extensions.GetDeltaValueOfModificatorForEachTurn(effects, StatEffectTypes.defence);
+
+        public int GetTimeToEndOfDefenceEffect() => Extensions.GetTimeToEndOfEffect(effects, StatEffectTypes.defence);
+
+        public bool GetDefenceEffectIsConstantly() => Extensions.GetEffectIsConstantly(effects, StatEffectTypes.defence);
+        #endregion
+
+        #region IAbleToApplyStrenghtModificator
+        public int GetStrenghtModificatorValue() => Extensions.GetModificatorValue(effects, StatEffectTypes.strenght);
+
+        public int GetDeltaOfStrenghtValueForEachTurn() => Extensions.GetDeltaValueOfModificatorForEachTurn(effects, StatEffectTypes.strenght);
+
+        public int GetTimeToEndOfStrenghtEffect() => Extensions.GetTimeToEndOfEffect(effects, StatEffectTypes.strenght);
+
+        public bool GetStrenghtEffectIsConstantly() => Extensions.GetEffectIsConstantly(effects, StatEffectTypes.strenght);
+        #endregion
+
+        #region IAbleToApplyIntelligenceModificator
+        public int GetIntelligenceModificatorValue() => Extensions.GetModificatorValue(effects, StatEffectTypes.intelegence);
+
+        public int GetDeltaOfIntelligenceValueForEachTurn() => Extensions.GetDeltaValueOfModificatorForEachTurn(effects, StatEffectTypes.intelegence);
+
+        public int GetTimeToEndOfIntelligenceEffect() => Extensions.GetTimeToEndOfEffect(effects, StatEffectTypes.intelegence);
+
+        public bool GetIntelligenceEffectIsConstantly() => Extensions.GetEffectIsConstantly(effects, StatEffectTypes.intelegence);
+        #endregion
+
+        #region IAbleToApplyDexterityModificator
+        public int GetDexterityModificatorValue() => Extensions.GetModificatorValue(effects, StatEffectTypes.dexterity);
+
+        public int GetDeltaOfDexterityValueForEachTurn() => Extensions.GetDeltaValueOfModificatorForEachTurn(effects, StatEffectTypes.dexterity);
+
+        public int GetTimeToEndOfDexterityEffect() => Extensions.GetTimeToEndOfEffect(effects, StatEffectTypes.dexterity);
+
+        public bool GetDexterityEffectIsConstantly() => Extensions.GetEffectIsConstantly(effects, StatEffectTypes.dexterity);
+        #endregion
+
+        #region IAbleToApplySpikesModificator
+        public int GetSpikesModificatorValue() => Extensions.GetModificatorValue(effects, StatEffectTypes.spikes);
+
+        public int GetdeltaOfSpikesValueForEachTurn() => Extensions.GetDeltaValueOfModificatorForEachTurn(effects, StatEffectTypes.spikes);
+
+        public int GetTimeToEndOfSpikesEffect() => Extensions.GetTimeToEndOfEffect(effects, StatEffectTypes.spikes);
+
+        public bool GetSpikesEffectIsConstantly() => Extensions.GetEffectIsConstantly(effects, StatEffectTypes.spikes);
+        #endregion
+
+        #region IAbleToApplyImmobilizedModificator
+        public int GetTimeToEndOfImmpobilizedEffect() => Extensions.GetTimeToEndOfEffect(effects, StatEffectTypes.spikes);
+        #endregion
+
+        #region IAbleToApplyBlock
+        public int GetDefaultBlockValue() => temporaryBlockValue;
+
+        public int CalculateBlock() => Extensions.CalculateBlockWithGameRules(temporaryBlockValue, _owner.Stats);
+        #endregion
+
+        #region IAbleToApplyArmor
+        public int GetDefaultArmorValue() => permanentBlockValue;
+
+        public int CalculateArmor() => permanentBlockValue;
+        #endregion
     }
 }
