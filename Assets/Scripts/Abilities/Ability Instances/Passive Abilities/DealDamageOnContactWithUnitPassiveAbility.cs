8a3cca5117799d50eb044193c77d6b24d943b5e8
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-01-21 21:25:33 +0600

    an universal description for the deal damage on contact ability has been added

--- Assets/Scripts/Abilities/Ability Instances/Passive Abilities/DealDamageOnContactWithUnitPassiveAbility.cs
+++ Assets/Scripts/Abilities/Ability Instances/Passive Abilities/DealDamageOnContactWithUnitPassiveAbility.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using static UnityEngine.UI.CanvasScaler;
 
 namespace FroguesFramework
 {
-    public class DealDamageOnContactWithUnitPassiveAbility : PassiveAbility
+    public class DealDamageOnContactWithUnitPassiveAbility : PassiveAbility, IAbleToReturnSingleValue
     {
         [SerializeField] private int damage;
+
+        public int GetValue() => damage;
+
         public override void Init(Unit unit)
         {
             base.Init(unit);
 
             unit.OnStepOnThisUnitByUnit.AddListener(DealDamage);
             unit.Movable.OnBumpIntoUnit.AddListener(DealDamage);
+        }
+
+        public override void UnInit()
+        {
+            _owner.OnStepOnThisUnitByUnit.RemoveListener(DealDamage);
+            _owner.Movable.OnBumpIntoUnit.RemoveListener(DealDamage);
+
+            base.UnInit();
         }
 
         private void DealDamage(Unit unit)
