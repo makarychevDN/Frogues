14a7a6f0178d5ae7d9fa910d3c641557ac9313f0
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-01-21 23:47:44 +0600

    an universal description for the apply block and armor on contact ability has been added

---
+++ Assets/Scripts/Abilities/Ability Instances/Passive Abilities/IncreaseBlockAndArmorToUnitOnConactPassiveAbility.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace FroguesFramework 
+{ 
+    public class IncreaseBlockAndArmorToUnitOnConactPassiveAbility : PassiveAbility, IAbleToApplyArmor, IAbleToApplyBlock
+    {
+        [SerializeField] private int blockValue;
+        [SerializeField] private int armorValue;
+
+        public int CalculateArmor() => armorValue;
+
+        public int CalculateBlock() => Extensions.CalculateBlockWithGameRules(blockValue, _owner.Stats);
+
+        public int GetDefaultArmorValue() => armorValue;
+
+        public int GetDefaultBlockValue() => blockValue;
+
+        public override void Init(Unit unit)
+        {
+            base.Init(unit);
+
+            unit.OnStepOnThisUnitByUnit.AddListener(IncreaseArmorAndBlockToUnit);
+            unit.Movable.OnBumpIntoUnit.AddListener(IncreaseArmorAndBlockToUnit);
+        }
+
+        private void IncreaseArmorAndBlockToUnit(Unit unit)
+        {
+            unit.Health.IncreasePermanentBlock(armorValue);
+        }
+    }
+}
