4a010cf10c77301cf8041fe541b87929b45c05cf
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-02-18 15:41:26 +0600

    now player is able to cancel reward + graphics of rewards panel have been updated

--- Assets/Scripts/Rewards System/SetOfRewardsPanel.cs
+++ Assets/Scripts/Rewards System/SetOfRewardsPanel.cs
@@ -1,18 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace FroguesFramework
 {
     public class SetOfRewardsPanel : MonoBehaviour
     {
-        [SerializeField] private List<BaseAbility> ableToBeChosenAbilities;
         [SerializeField] private SingleRewardPanel singleRewardPanelPrefab;
         [SerializeField] private Transform parentForSignleRewardPanels;
+        [SerializeField] private Button cancelButton;
+        [SerializeField] private List<BaseAbility> ableToBeChosenAbilities;
         public UnityEvent OnRewardFromSetPicked;
 
         public void Init(bool isPassiveAbilitiesPool, int numberOfAbilitesAbleToChoose)
         {
+            cancelButton.onClick.AddListener(InvokeRewardPickedEvent);
+
             var abilityList = isPassiveAbilitiesPool ? EntryPoint.Instance.PoolOfPassiveAbilitiesPerRun : EntryPoint.Instance.PoolOfActiveAbilitiesPerRun;
 
             for(int i = 0; i < numberOfAbilitesAbleToChoose; i++)
@@ -25,8 +29,10 @@ namespace FroguesFramework
                 singleRewardPanel.transform.parent = parentForSignleRewardPanels;
                 singleRewardPanel.transform.SetSiblingIndex(1);
                 singleRewardPanel.Init(abilty, EntryPoint.Instance.MetaPlayer);
-                singleRewardPanel.OnRewardPicked.AddListener(() => OnRewardFromSetPicked.Invoke());
+                singleRewardPanel.OnRewardPicked.AddListener(InvokeRewardPickedEvent);
             }
         }
+
+        private void InvokeRewardPickedEvent() => OnRewardFromSetPicked.Invoke();
     }
 }
