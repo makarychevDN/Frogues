d71e13fbf8588bd00ba4e6b8acef472936649ffe
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-02-19 15:40:13 +0600

    a get rewards button has been added

--- Assets/Scripts/Rewards System/RewardsMenu.cs
+++ Assets/Scripts/Rewards System/RewardsMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 using static FroguesFramework.RewardsGenerator;
 
 namespace FroguesFramework
@@ -9,16 +10,22 @@ namespace FroguesFramework
     {
         [SerializeField] private Transform parentForSetsOfRewards;
         [SerializeField] private GameObject background;
+        [SerializeField] private Button chooseRewardsButton;
         [SerializeField] private SetOfRewardsPanel setOfRewardsPanelPrefab;
         [SerializeField] private List<SetOfRewardsPanel> setOfRewardsPanels;
         public UnityEvent<SetOfRewardsPanel> OnRewardChosen;
+
+        public void Awake()
+        {
+            chooseRewardsButton.onClick.AddListener(DisplayRewardsMenu);
+        }
 
         public void GenerateSetOfRewards(RewardType rewardType, int countOfPossibleRewards)
         {
             var newSetOfRewardsPanel = Instantiate(setOfRewardsPanelPrefab, parentForSetsOfRewards);
             newSetOfRewardsPanel.Init(rewardType, countOfPossibleRewards);
             setOfRewardsPanels.Add(newSetOfRewardsPanel);
-            background.SetActive(true);
+            chooseRewardsButton.gameObject.SetActive(true);
             newSetOfRewardsPanel.OnRewardFromSetPicked.AddListener(() => RemoveSetOfRewardsFromPanel(newSetOfRewardsPanel));
         }
 
@@ -26,7 +33,15 @@ namespace FroguesFramework
         {
             setOfRewardsPanels.Remove(setOfRewards);
             Destroy(setOfRewards.gameObject);
+            parentForSetsOfRewards.gameObject.SetActive(setOfRewardsPanels.Count > 0);
             background.SetActive(setOfRewardsPanels.Count > 0);
+        }
+
+        private void DisplayRewardsMenu()
+        {
+            parentForSetsOfRewards.gameObject.SetActive(true);
+            background.SetActive(true);
+            chooseRewardsButton.gameObject.SetActive(false);
         }
     }
 }
