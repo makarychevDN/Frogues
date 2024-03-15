0d842a10254264cd5c68e342b03c3afc25b213ba
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-03-15 17:15:06 +0500

    now a shadow o an unity updates her x angle due a camera rotation angle

--- Assets/Scripts/LookAtTheCamera.cs
+++ Assets/Scripts/LookAtTheCamera.cs
@@ -4,17 +4,12 @@ namespace FroguesFramework
 {
     public class LookAtTheCamera : MonoBehaviour
     {
-        private Transform camera;
-
-        void Start()
-        {
-            camera = Camera.main.transform;
-        }
+        [SerializeField] private Transform shadow;
 
         void Update()
         {
-            //transform.LookAt(camera);
-            transform.rotation = Quaternion.LookRotation(camera.transform.forward);
+            transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
+            shadow.localEulerAngles = new Vector3(90 - transform.localEulerAngles.x, 0, 0);
         }
 
     }
