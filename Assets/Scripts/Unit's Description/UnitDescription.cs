8625d6a8ffd3c2e6dac6833bb3d0b276740369a9
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-04-12 21:47:20 +0500

    names and descriptions of units have been localized

--- Assets/Scripts/Unit's Description/UnitDescription.cs
+++ Assets/Scripts/Unit's Description/UnitDescription.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
+using UnityEngine.Localization;
 
 namespace FroguesFramework
 {
     public class UnitDescription : MonoBehaviour
     {
-        [SerializeField] private string unitName;
-        [SerializeField, TextArea] private string description;
+        [SerializeField] private LocalizedString unitName;
+        [SerializeField] private LocalizedString description;
 
-        public string UnitName => unitName;
-        public string Description => description;
+        public string UnitName => unitName.GetLocalizedString();
+        public string Description => description.GetLocalizedString();
     }
 }
