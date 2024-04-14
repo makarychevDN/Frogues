615778df4a1e14d3c08eeab70636a057339a2d31
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-04-14 17:19:23 +0500

    new weapon setter works have been started + graphics of weapin racks have been added

--- Assets/Scripts/LookAtTheCamera.cs
+++ Assets/Scripts/LookAtTheCamera.cs
@@ -5,11 +5,20 @@ namespace FroguesFramework
     public class LookAtTheCamera : MonoBehaviour
     {
         [SerializeField] private Transform shadow;
+        [SerializeField] private bool blockXRotation = false;
 
         void Update()
         {
             transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
-            shadow.localEulerAngles = new Vector3(90 - transform.localEulerAngles.x, 0, 0);
+
+            if (blockXRotation)
+            {
+                transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, transform.localEulerAngles.z);
+            }
+
+
+            if(shadow != null)
+                shadow.localEulerAngles = new Vector3(90 - transform.localEulerAngles.x, 0, 0);
         }
 
     }
