1c66d3e45f4b93ed927c22ab75506e483874b66c
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-04-16 23:40:57 +0500

    score hinrs have been extended

--- Assets/Scripts/UI/XPSlidersPanel.cs
+++ Assets/Scripts/UI/XPSlidersPanel.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Localization;
 using UnityEngine.UI;
 using static FroguesFramework.RewardsGenerator;
 
@@ -11,6 +12,21 @@ namespace FroguesFramework
         [SerializeField] Slider lvlUpScrollbar;
         [SerializeField] GameObject iconOfLvlUp;
         [SerializeField] GameObject iconOfMaxLvl;
+
+        [SerializeField] LocalizedString finalTestName;
+        [SerializeField] LocalizedString finalTestDescription;
+
+        [SerializeField] LocalizedString campName;
+        [SerializeField] LocalizedString campDescription;
+
+        [SerializeField] LocalizedString levelName;
+        [SerializeField] LocalizedString levelDescription;
+        [SerializeField] LocalizedString levelDescriptionOnMaxLvl;
+
+        [SerializeField] private Vector2 pivot;
+        [SerializeField] private Vector2 offset;
+
+        private int _hashedPointsToGoToCamp;
 
         private void Start()
         {
@@ -30,7 +46,7 @@ namespace FroguesFramework
             RewardPanelSetup nextReward = EntryPoint.Instance.RewardsGenerator.GetNextRewardSetup();
             RewardPanelSetup lastReward = EntryPoint.Instance.RewardsGenerator.GetLastRewardSetup();
 
-            if(nextReward == null)
+            if (nextReward == null)
             {
                 iconOfLvlUp.SetActive(false);
                 iconOfMaxLvl.SetActive(true);
@@ -50,6 +66,46 @@ namespace FroguesFramework
         {
             campfireScrollbar.maxValue = EntryPoint.Instance.AscensionSetup.RequaredDeltaOfScoreToOpenExitToCampfire;
             campfireScrollbar.value = EntryPoint.Instance.ScoreDeltaCounterForBonfire;
+            _hashedPointsToGoToCamp = EntryPoint.Instance.AscensionSetup.RequaredDeltaOfScoreToOpenExitToCampfire - EntryPoint.Instance.ScoreDeltaCounterForBonfire;
+        }
+
+        public void ShowHintAboutCampfire()
+        {
+            string campDescriptionWithValues = campDescription.GetLocalizedString();
+            campDescriptionWithValues = campDescriptionWithValues.Replace("{value}", EntryPoint.Instance.AscensionSetup.RequaredDeltaOfScoreToOpenExitToCampfire.ToString());
+            campDescriptionWithValues = campDescriptionWithValues.Replace("{value_2}", _hashedPointsToGoToCamp.ToString());
+            EntryPoint.Instance.CommonSmallHint.Init(campName.GetLocalizedString(), campDescriptionWithValues, transform, pivot, offset);
+            EntryPoint.Instance.CommonSmallHint.EnableContent(true);
+        }
+
+        public void ShowHintAboutLvlUp()
+        {
+            string levelDescriptionWithValues;
+
+            var nextReward = EntryPoint.Instance.RewardsGenerator.GetNextRewardSetup();
+            if (nextReward == null)
+            {
+                levelDescriptionWithValues = levelDescriptionOnMaxLvl.GetLocalizedString();
+            }
+            else
+            {
+                levelDescriptionWithValues = levelDescription.GetLocalizedString();
+                levelDescriptionWithValues = levelDescriptionWithValues.Replace("{value}", nextReward.scoreRequirement.ToString());
+            }
+
+            EntryPoint.Instance.CommonSmallHint.Init(levelName.GetLocalizedString(), levelDescriptionWithValues, transform, pivot, offset);
+            EntryPoint.Instance.CommonSmallHint.EnableContent(true);
+        }
+
+        public void ShowHintAboutFinalTest()
+        {
+            EntryPoint.Instance.CommonSmallHint.Init(finalTestName.GetLocalizedString(), finalTestDescription.GetLocalizedString(), transform, pivot, offset);
+            EntryPoint.Instance.CommonSmallHint.EnableContent(true);
+        }
+
+        public void HideHint()
+        {
+            EntryPoint.Instance.CommonSmallHint.EnableContent(false);
         }
     }
 }
