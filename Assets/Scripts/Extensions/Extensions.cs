8cb8148513dce6ecb0a3fbe4ee350d03464ac03f
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-04-22 00:04:26 +0500

    a new layout of the main menu with a new logo

--- Assets/Scripts/Extensions/Extensions.cs
+++ Assets/Scripts/Extensions/Extensions.cs
@@ -317,5 +317,25 @@ namespace FroguesFramework
 
             return stringBuilder.ToString();
         }
+
+        public static float GetTrailLength(this TrailRenderer trailRenderer)
+        {
+            var points = new Vector3[trailRenderer.positionCount]; 
+            var count = trailRenderer.GetPositions(points);
+
+            if (count < 2) return 0f;
+
+            var length = 0f;
+            var start = points[0];
+
+            for (var i = 1; i < count; i++)
+            {
+                var end = points[i];
+                length += Vector3.Distance(start, end);
+                start = end;
+            }
+
+            return length;
+        }
     }
 }
