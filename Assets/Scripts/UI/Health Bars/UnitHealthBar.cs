99948fc224365511d1de0d226324dbc56289c83a
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-05-06 22:39:34 +0500

    a visualisation of escapes from death has been added for units

--- Assets/Scripts/UI/Health Bars/UnitHealthBar.cs
+++ Assets/Scripts/UI/Health Bars/UnitHealthBar.cs
@@ -8,6 +8,7 @@ namespace FroguesFramework
         [SerializeField] private GameObject blockIcon;
         [SerializeField] private GameObject armorIcon;
         [SerializeField] private GameObject spikesIcon;
+        [SerializeField] private GameObject escapeFromDeathIcon;
 
         [SerializeField] private IntSpriteFontSegment healthTextField;
         [SerializeField] private IntSpriteFontSegment healthTextFieldPretakenAnimation;
@@ -18,6 +19,9 @@ namespace FroguesFramework
         [SerializeField] private IntSpriteFontSegment armorTextField;
         [SerializeField] private IntSpriteFontSegment armorTextFieldPretakenAnimation;
 
+        [SerializeField] private IntSpriteFontSegment escapeFromDeathIconTextField;
+        [SerializeField] private IntSpriteFontSegment escapeFromDeathIconTextFieldPretakenAnimation;
+
         [SerializeField] private IntSpriteFontSegment spikesTextField;
 
         public override void Redraw()
@@ -27,6 +31,7 @@ namespace FroguesFramework
             blockIcon.SetActive(health.Block != 0);
             armorIcon.SetActive(health.Armor != 0);
             spikesIcon.SetActive(stats.Thorns != 0);
+            escapeFromDeathIcon.SetActive(health.EscapesFromDeath != 0);
 
             healthTextField.SetValue(health.HealthWithPreTakenDamage);
             healthTextFieldPretakenAnimation.SetValue(health.HealthWithPreTakenDamage);
@@ -39,6 +44,10 @@ namespace FroguesFramework
             blockTextField.SetValue(health.BlockWithPreTakenDamage);
             blockTextFieldPretakenAnimation.SetValue(health.BlockWithPreTakenDamage);
             blockTextFieldPretakenAnimation.gameObject.SetActive(health.BlockWithPreTakenDamage != health.Block);
+
+            escapeFromDeathIconTextField.SetValue(health.EscapesFromDeathCountWithPretakenDamage);
+            escapeFromDeathIconTextFieldPretakenAnimation.SetValue(health.EscapesFromDeathCountWithPretakenDamage);
+            escapeFromDeathIconTextFieldPretakenAnimation.gameObject.SetActive(health.EscapesFromDeathCountWithPretakenDamage != health.EscapesFromDeath);
 
             spikesTextField.SetValue(stats.Thorns);
 
