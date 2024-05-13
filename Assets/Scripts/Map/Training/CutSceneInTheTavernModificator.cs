072d98fc24a832044d17261c0134b140f4aff398
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-05-13 20:36:08 +0500

    the second version on the cut scene

--- Assets/Scripts/Map/Training/CutSceneInTheTavernModificator.cs
+++ Assets/Scripts/Map/Training/CutSceneInTheTavernModificator.cs
@@ -5,11 +5,13 @@ namespace FroguesFramework
 {
     public class CutSceneInTheTavernModificator : BaseTrainingModificator
     {
-        [SerializeField] private Vector3 cameraStartPosition = new Vector3(7.64f, 1.13f, 4.0f);
         [SerializeField] private Transform movementTrailObject;
+        [SerializeField] private GameObject enemiesSprite;
 
         public override void Init()
         {
+            Cursor.visible = false;
+            enemiesSprite.SetActive(false);
             CurrentlyActiveObjects.Add(this);
             Camera.main.transform.parent = movementTrailObject;
             Camera.main.transform.localPosition = Vector3.zero;
@@ -17,6 +19,12 @@ namespace FroguesFramework
             FindObjectOfType<PlayerInput>().enabled = false;
             FindObjectsOfType<Unit>().ToList().ForEach(unit => unit.gameObject.SetActive(false));
             FindObjectsOfType<Canvas>().ToList().ForEach(canvas => canvas.gameObject.SetActive(false));
+            Invoke(nameof(TurnOnEnemies), 1.5f);
+        }
+
+        private void TurnOnEnemies()
+        {
+            enemiesSprite.SetActive(true);
         }
     }
 }
