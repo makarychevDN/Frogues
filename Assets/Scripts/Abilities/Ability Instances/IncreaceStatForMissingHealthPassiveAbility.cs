b95f4614b0783c9ac7f5ca367900a4b7fd848777
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-05-16 21:03:36 +0500

    bugfix with a wrong working ability

--- Assets/Scripts/Abilities/Ability Instances/IncreaceStatForMissingHealthPassiveAbility.cs
+++ Assets/Scripts/Abilities/Ability Instances/IncreaceStatForMissingHealthPassiveAbility.cs
@@ -17,7 +17,7 @@ namespace FroguesFramework
         public override void Init(Unit unit)
         {
             base.Init(unit);
-            statEffect = new StatEffect(statEffectType, additionalStrenghtValue, 0, 0, true);
+            statEffect = new StatEffect(statEffectType, additionalStrenghtValue, 1, 0, true);
             _owner.Stats.AddStatEffect(statEffect);
             _owner.Health.OnDamageAppledByHealth.AddListener(RecalculateStrenght);
             _owner.Health.OnHpHealed.AddListener(RecalculateStrenght);
