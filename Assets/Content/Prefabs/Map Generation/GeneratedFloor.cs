c3ab7c62b5f24fb37c353c921acd83a08666d36b
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-06-23 20:39:27 +0500

    new floors have been added

---
+++ Assets/Content/Prefabs/Map Generation/GeneratedFloor.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace FroguesFramework
+{
+    public class GeneratedFloor : BaseFloor
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
