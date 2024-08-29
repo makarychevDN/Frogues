bbc82e73882f228632844567082a26cb54ee366f
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-08-29 19:28:26 +0500

    Stats.cs has been restored to make changes more smoothly

--- Assets/Scripts/Unit's Stuff/Stats.cs
+++ Assets/Scripts/Unit's Stuff/Stats.cs
@@ -1,15 +1,206 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace FroguesFramework
 {
-    public class Stats : MonoBehaviour
+    public class Stats : MonoBehaviour, IAbleToCalculateHashFunctionOfPrevisualisation, IRoundTickable
     {
-        public int Thorns => 1;
+        [SerializeField] private List<StatEffect> strenght;
+        [SerializeField] private List<StatEffect> intelegence;
+        [SerializeField] private List<StatEffect> dexterity;
+        [SerializeField] private List<StatEffect> defence;
+        [SerializeField] private List<StatEffect> thorns;
+        [SerializeField] private List<StatEffect> immobilized;
+        [SerializeField] private float strengtModificatorStep;
+        [SerializeField] private float intelegenceModificatorStep;
+        [SerializeField] private float dexterityModificatorStep;
+        [SerializeField] private float defenceModificatorStep;
+        public UnityEvent<StatEffectTypes, int> OnStrenghtUpdated, OnIntelegenceUpdated, OnDexterityUpdated, OnDefenceUpdated, OnSpikesUpdated, OnImmobilizedUpdated;
+        public UnityEvent OnSomethingUpdated;
+        private Unit _owner;
+        private Dictionary<StatEffectTypes, List<StatEffect>> _statsDictionary = new();
+        private Dictionary<StatEffectTypes, UnityEvent<StatEffectTypes, int>> _statsUpdatedEventsDictionary = new();
 
-        public void Init(Unit owner)
+        public int Strenght => strenght.GetStatValue();
+        public int Intelegence => intelegence.GetStatValue();
+        public int Dexterity => dexterity.GetStatValue();
+        public int Defence => defence.GetStatValue();
+        public int Thorns => thorns.GetStatValue();
+        public int Immobilized => immobilized.GetTimeToTheEndOfEffect();
+
+        public float StrenghtModificator => (1 + strenght.GetStatValue() * strengtModificatorStep);
+        public float IntelegenceModificator => (1 + intelegence.GetStatValue() * intelegenceModificatorStep);
+        public float StrenghtAndIntelligenceSumModificator => (1 + strenght.GetStatValue() * strengtModificatorStep + intelegence.GetStatValue() * intelegenceModificatorStep);
+        public float DexterityeModificator => (1 + dexterity.GetStatValue() * dexterityModificatorStep);
+        public float DefenceModificator => (1 - defence.GetStatValue() * defenceModificatorStep);
+
+        public float StrenghtModificatorPersentages => strengtModificatorStep * 100;
+        public float IntelegenceModificatorPersentages => intelegenceModificatorStep * 100;
+        public float DexterityeModificatorPersentages => dexterityModificatorStep * 100;
+        public float DefenceModificatorPersentages => defenceModificatorStep * 100;
+
+        public int CalculateHashFunctionOfPrevisualisation() => strenght.GetStatValue() * 4 + intelegence.GetStatValue() * 40 + dexterity.GetStatValue() * 400 + defence.GetStatValue() * 4000 + thorns.GetStatValue() * 40000 + immobilized.GetTimeToTheEndOfEffect() * 400000;
+
+        public StatEffect AddStatEffect(StatEffectTypes type, int value, int timeToTheEndOfEffect, int deltaValueForEachTurn = 0, bool effectIsConstantly = false)
         {
+            StatEffect statEffect = new StatEffect(type, value, timeToTheEndOfEffect, deltaValueForEachTurn, effectIsConstantly);
+            _statsDictionary[type].Add(statEffect);
+            _statsUpdatedEventsDictionary[type].Invoke(type, value);
+            OnSomethingUpdated.Invoke();
+            statEffect.OnEffectValueChanged.AddListener(InvokeEventByKey);
+            return statEffect;
+        }
+
+        private void InvokeEventByKey(StatEffectTypes key, int value)
+        {
+            _statsUpdatedEventsDictionary[key].Invoke(key, value);
+            OnSomethingUpdated.Invoke();
+        }
+
+        public void AddStatEffect(StatEffect statEffect)
+        {
+            _statsDictionary[statEffect.type].Add(statEffect);
+            _statsUpdatedEventsDictionary[statEffect.type].Invoke(statEffect.type, statEffect.Value);
+            OnSomethingUpdated.Invoke();
+            statEffect.OnEffectValueChanged.AddListener(InvokeEventByKey);
+        }
+
+        public void RemoveStatEffect(StatEffect statEffect)
+        {
+            statEffect.OnEffectValueChanged.RemoveListener(InvokeEventByKey);
+            _statsDictionary[statEffect.type].Remove(statEffect);
+            _statsUpdatedEventsDictionary[statEffect.type].Invoke(statEffect.type, -statEffect.Value);
+            OnSomethingUpdated.Invoke();
+        }
+
+        public void RemoveAllNonConstantlyEffects()
+        {
+            foreach (var statEffectsList in _statsDictionary.Values)
+            {
+                statEffectsList.RemoveAll(statEffect => !statEffect.effectIsConstantly);
+            }
+        }
+
+        #region timerStuff
+        public void TickAfterEnemiesTurn()
+        {
+            if (_owner.IsEnemy)
+                return;
+
+            TickAllEffects();
+        }
+
+        public void TickAfterPlayerTurn()
+        {
+            if (!_owner.IsEnemy)
+                return;
+
+            TickAllEffects();
+        }
+
+        private void TickAllEffects()
+        {
+            foreach (var key in _statsDictionary.Keys)
+            {
+                for (int i = 0; i < _statsDictionary[key].Count; i++)
+                {
+                    _statsDictionary[key][i].Tick();
+
+                    if (_statsDictionary[key][i].timeToTheEndOfEffect <= 0)
+                    {
+                        RemoveStatEffect(_statsDictionary[key][i]);
+                        i--;
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region InitStuff
+        public void Init(Unit unit)
+        {
+            _owner = unit;
+            _statsDictionary = new Dictionary<StatEffectTypes, List<StatEffect>>
+            {
+                { StatEffectTypes.strength, strenght },
+                { StatEffectTypes.intelligence, intelegence },
+                { StatEffectTypes.dexterity, dexterity },
+                { StatEffectTypes.defence, defence },
+                { StatEffectTypes.thorns, thorns },
+                { StatEffectTypes.immobilized, immobilized }
+            };
+
+            _statsUpdatedEventsDictionary = new Dictionary<StatEffectTypes, UnityEvent<StatEffectTypes, int>>
+            {
+                { StatEffectTypes.strength, OnStrenghtUpdated },
+                { StatEffectTypes.intelligence, OnIntelegenceUpdated },
+                { StatEffectTypes.dexterity, OnDexterityUpdated },
+                { StatEffectTypes.defence, OnDefenceUpdated },
+                { StatEffectTypes.thorns, OnSpikesUpdated },
+                { StatEffectTypes.immobilized, OnImmobilizedUpdated }
+            };
+        }
+
+        public void UnInit() { }
+        #endregion
+
+    }
+
+    public enum StatEffectTypes
+    {
+        strength = 0,
+        intelligence = 10,
+        dexterity = 20,
+        defence = 30,
+        thorns = 40,
+        immobilized = 50
+    }
+
+    [Serializable]
+    public class StatEffect
+    {
+        public StatEffectTypes type;
+        [SerializeField] private int value;
+        public int deltaValueForEachTurn;
+        public int timeToTheEndOfEffect;
+        public bool effectIsConstantly;
+        public UnityEvent<StatEffectTypes, int> OnEffectValueChanged;
+
+        public StatEffect(StatEffectTypes type, int value, int timeToTheEndOfEffect, int deltaForEachTurn = 0, bool effectIsConstantly = false)
+        {
+            this.type = type;
+            this.value = value;
+            this.timeToTheEndOfEffect = timeToTheEndOfEffect;
+            this.effectIsConstantly = effectIsConstantly;
+            this.deltaValueForEachTurn = deltaForEachTurn;
+            OnEffectValueChanged = new UnityEvent<StatEffectTypes, int>();
+        }
+
+        public StatEffect(StatEffect statEffect) : this(statEffect.type, statEffect.value, statEffect.timeToTheEndOfEffect, statEffect.deltaValueForEachTurn, statEffect.effectIsConstantly) { }
+
+        public void Tick(int ticksValue = 1)
+        {
+            if (!effectIsConstantly)
+                timeToTheEndOfEffect -= ticksValue;
+
+            Value += deltaValueForEachTurn;
+        }
+
+        public int Value
+        {
+            get { return value; }
+
+            set
+            {
+                int delta = value - this.value;
+                this.value = value;
+
+                if (delta != 0)
+                    OnEffectValueChanged.Invoke(type, delta);
+            }
         }
     }
 }
